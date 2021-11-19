1bff6ef5de29e4a1edebaa09d8861053dd1659b1
Firdausdimas <93905908+firdausdimas@users.noreply.github.com>
2021-11-19 17:59:06 +0700

    Delete Turkey Covid-19 directory

--- Turkey Covid-19/Join Percentage Data Vaccination 1-2 Doses(1).sql
+++
@@ -1,7 +0,0 @@
-#--- Join Percentage Data Vaccination ---#
-SELECT ID, CITY, _1DOSE, _2DOSE, _TOTAL, POPULATION, 
-       ROUND(_1DOSE * 100.0 / POPULATION, 1) AS 1_dose_perc,
-       ROUND(_2DOSE * 100.0 / POPULATION, 1) AS 2_dose_perc
-FROM dimassql.covid19vaccination
-ORDER BY CITY, _1DOSE, 1_dose_perc, 2_dose_perc;
-#----------------------------------------#
--- Turkey Covid-19/Vaccination Rates by City 2nd.sql
+++
@@ -1,8 +0,0 @@
-#--- Vaccination Rates by City ---#
-#--- Vaccination Rates by City (2nd Dose) ---#
-SELECT ID, CITY, _2DOSE, _TOTAL, POPULATION, 
-       ROUND(_2DOSE * 100.0 / POPULATION, 1) AS 2_dose_perc
-FROM dimassql.covid19vaccination
-ORDER BY 2_dose_perc DESC;
-#--------------------------------------------#
-#---------------------------------#
